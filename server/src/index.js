338e208b55e08092a95fd8e7de19daf193735266
Fatih Şen <fatihydrm@outlook.com>
2022-11-18 22:25:11 +0300

    connect db and routing

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 import mongoose from "mongoose";
 import cors from "cors";
+import usersRouter from "./routes/users/users.js";
+import postsRouter from "./routes/posts/posts.js";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 dotenv.config();
@@ -12,12 +14,21 @@ app.use(cors());
 
 const port = process.env.port || 5000;
 
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+
 app.listen(port, () => {
   console.log(`Server listen ${port}`);
 });
 
+(async () => {
+  try {
+    await mongoose.connect(process.env.DBURL);
+    console.log("connected mongodb");
+  } catch (err) {
+    console.log(err);
+  }
+})();
 app.get("/", (req, res) => {
-  console.log("Başarılı");
-
   res.send({ message: "success" });
 });
