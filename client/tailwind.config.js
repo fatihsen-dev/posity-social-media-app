439777126553d025cad991a1f5930db591540236
Fatih Şen <fatihydrm@outlook.com>
2022-11-24 23:35:24 +0300

    update

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
    content: ["./src/**/*.{js,jsx,ts,tsx}"],
    theme: {
-      extend: {
-         fontFamily: {
-            sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-         },
-      },
+      extend: {},
       container: {
          center: true,
       },
