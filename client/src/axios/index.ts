b1c77411bd5eec87f035a101daa8687ce7187a6b
Fatih Şen <fatihydrm@outlook.com>
2022-11-24 23:36:33 +0300

    add new request

--- client/src/axios/index.ts
+++ client/src/axios/index.ts
@@ -1,21 +1,29 @@
 import axios from "axios";
-
 const HTTP = axios.create({
    baseURL: process.env.REACT_APP_API_URL,
 });
 
+// user login
 export const login = async (formData: object) =>
    await HTTP.post("/users/login", formData);
 
+// user login control
 export const Control = async (token: object) =>
    await HTTP.post("/users/usercontrol", token);
 
+// user register
 export const register = async (formData: object) =>
    await HTTP.post("/users/register", formData);
 
+// get all users
 export const getAllUsers = async () => await HTTP.get("/users");
 
+// get profile page data
 export const getProfileUser = async (userid: any) =>
    await HTTP.get(`/users/getoneuser/${userid}`);
 
-export const getAllPost = async () => await HTTP.post("/posts");
+// New post
+export const newPost = async (formData: object) =>
+   await HTTP.post("/posts/create", formData);
+
+export const getAllPost = async () => await HTTP.get("/posts");
