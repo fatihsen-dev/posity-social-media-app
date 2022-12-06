62dcb492726fd492533165c6f6cc91d534369870
Fatih Şen <fatihydrm@outlook.com>
2022-12-07 01:19:34 +0300

    fix router bug

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,5 +1,18 @@
 import { Player } from "@lottiefiles/react-lottie-player";
-export default function Loading() {
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+interface TLoading {
+   user?: null | false | object;
+}
+
+export default function Loading({ user }: TLoading) {
+   const navigate = useNavigate();
+
+   useEffect(() => {
+      user === false && navigate("login");
+   }, [user]);
+
    return (
       <div className='absolute inset-0 z-10 grid w-full h-full bg-lightV1 place-items-center'>
          <Player
